5d0274f0965c64350e5fb53c4930c95a0bf748cb
sherly <sharton1@asu.edu>
2022-08-18 21:56:22 -0400

    add socket

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, Response, request
 from flask_cors import CORS
 import json
-from face_rec import FaceRec, sherly
 from PIL import Image
 import base64
 import io
@@ -9,7 +8,7 @@ import cv2
 import numpy as np
 
 #socket import
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 CORS(app)
@@ -17,13 +16,36 @@ CORS(app)
 # socket - get message
 socketIo = SocketIO(app, cors_allowed_origins="*")
 @socketIo.on("message")  
-def handleMessage(msg):
-    print("handling message", msg)
-    send(msg, broadcast = True)
-    return None
-
-
-
+def handleMessage(data):
+    
+    result_str = data
+    if(not result_str):
+        print("no result")
+    else:
+        # 2. convert to base to PILimage then OpenCV frame
+        b = bytes(result_str, 'utf-8')
+        image = b[b.find(b'/9'):]
+        pilImg = Image.open(io.BytesIO(base64.b64decode(image)))
+        frame = cv2.cvtColor(np.array(pilImg), cv2.COLOR_BGR2RGB)
+    
+		# 3. write on frame
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        coord = (50, 50)
+        fontScale = 1
+        color = (255, 0, 0)
+        thickness = 2
+        frame = cv2.putText(frame, 'OpenCV', coord, font,
+							fontScale, color, thickness, cv2.LINE_AA)
+        # 4. convert opencv frame to base64 string
+        retval, buffer = cv2.imencode('.jpg', frame)
+        base64String = base64.b64encode(buffer)
+        
+        # print("type>>>",type(base64String))
+        json_data = {'data': base64String}
+        emit('message', json_data, broadcast=True)
+        # send({data: base64String}, broadcast=True, json=True)
+        # send(data, broadcast=True)
+        
 # http
 @app.route('/api', methods=['POST', 'GET'])
 def api():
@@ -46,7 +68,7 @@ def api():
 	frame = cv2.putText(frame, 'OpenCV', coord, font,
                      fontScale, color, thickness, cv2.LINE_AA)
 
-	# 4. convert opencv frame to base64 string
+    # 4. convert opencv frame to base64 string
 	retval, buffer = cv2.imencode('.jpg', frame)
 	base64String = base64.b64encode(buffer)
  
